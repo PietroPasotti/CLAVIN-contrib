4700593f3724328b2e784db7e5bf8d9ce00951a2
Travis L Pinney <tpinney@bericotechnologies.com>
2013-10-02 14:37:14 -0400

    Add api

--- gdelt/gdelt/gdelt/urls.py
+++ gdelt/gdelt/gdelt/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import patterns, include, url
 
+from gdelt.api import GDELTResource
+
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+gdelt_resource = GDELTResource()
+
+
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'gdelt.views.home', name='home'),
-    # url(r'^gdelt/', include('gdelt.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^api/', include(gdelt_resource.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
