919d0ab5aea7a68a5b7bc38331e25896bc262dcc
Liz Merkhofer <lmerkhofer@bericotechnologies.com>
2013-10-25 16:39:00 -0400

    country tools

--- clavin-python/clavin/__init__.py
+++ clavin-python/clavin/__init__.py
@@ -1,23 +1,22 @@
 import requests 
-import code
-import json
+from collections import Counter
+from pprint import pprint
 
 
 class Clavin:
 
     def __init__(self, server):
         self.server = server
-        self.results = {}
+        self.resolved = {}
 
 
     def locationsDictionary(self):
-        return self.results['locations']
+        return self.resolved['locations']
 
     def whichClavin(self):
         return self.version
         
-
-
+#    def parse(self, document):
     #def connect(self, server):
     #    self.server = server 
     #    # connect to web server, should return basic metadata to verify it is working 
@@ -28,17 +27,30 @@ class Clavin:
         results = r.json()
         self.resolvedLocations = [resolvedLocation(record) for record in results['locations']]
         self.version = results['version']
-        self.results = results
+        self.resolved = results
         return results
 
     def toString(self):
-        formatted = u"{name}\t{countryName}\t{admin1Code}\t[pop: {population}]\tid: {geonameID}\n"
-        resolved = self.results['locations']
+        formatted = u"{name}\t{countryName}\tadmin 1 code: {admin1Code}\tpop: {population}\tid: {geonameID}\n"
+        resolved = self.resolved['locations']
         for loc in resolved:
             print formatted.format(**loc)
 
     def whichCountries(self):
-        return list(set(location.countryName for location in self.resolvedLocations)) 
+        countries = [location.countryName for location in self.resolvedLocations]
+        howmany = Counter(countries)
+        for country,occs in howmany.iteritems():
+           print("{}\t\t\t\t{} times".format(country,occs))
+        return howmany  
+
+    def locationsByCountry(self):
+        loc_by_country = {}
+        for country in set([location.countryName for location in self.resolvedLocations]):
+            loc_by_country[country] = list(set([location.name for location in self.resolvedLocations if location.countryName == country]))
+#This does not actually print very pretty
+#       pprint(loc_by_country)  
+        return loc_by_country
+        
 
 
 #    def __unicode__(self):    
