af1e69958d0a7d2fbc2c2059601ed44e3c4cdeb4
Liz Merkhofer <lmerkhofer@bericotechnologies.com>
2013-10-25 13:54:57 -0400

    python, get more from server

--- clavin-python/clavin/__init__.py
+++ clavin-python/clavin/__init__.py
@@ -1,22 +1,58 @@
 import requests 
-
-
+import code
+import json
 
 
 class Clavin:
 
     def __init__(self, server):
-        self.server = server  
+        self.server = server
+        self.results = {}
+
+
+    def locationsDictionary(self):
+        return self.results['locations']
+
+    def whichClavin(self):
+        return self.version
+        
+
 
     #def connect(self, server):
     #    self.server = server 
     #    # connect to web server, should return basic metadata to verify it is working 
 
-
     def resolve(self, document):
-	headers = {'content-type': 'text/plain'}
+        headers = {'content-type': 'text/plain'}
         r = requests.post(self.server, data=document, headers=headers)
-        return r.json() 
+        results = r.json()
+        self.resolvedLocations = [resolvedLocation(record) for record in results['locations']]
+        self.version = results['version']
+        self.results = results
+        return results
 
+    def toString(self):
+        formatted = u"{name}\t{countryName}\t{admin1Code}\t[pop: {population}]\tid: {geonameID}\n"
+        resolved = self.results['locations']
+        for loc in resolved:
+            print formatted.format(**loc)
+
+    def whichCountries(self):
+        return list(set(location.countryName for location in self.resolvedLocations)) 
+
+
+#    def __unicode__(self):    
+
+
+class resolvedLocation:
+
+    def __init__(self,record):
+        self.name = record['name']
+        self.countryName = record['countryName']
+        self.admin1Code = record['admin1Code']
+        self.population = record['population']
+        self.geonameID = record['geonameID']
+        self.latitude = record['latitude']
+        self.longitude = record['longitude']
 
  
